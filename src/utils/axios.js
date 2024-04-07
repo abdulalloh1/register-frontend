a7f2094429a6875d8eb124a3fa31f6e99362ba0f
Jasurbek Abdukhalikov <jasurbek.abdukhalikov@gmail.com>
2024-04-07 23:17:11 +0500

    update env

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = 'http://localhost:8800/api';
+const baseURL = import.meta.env.VITE_API_URL;
 
 const appAxios = axios.create({
     baseURL,
--- src/utils/config.js
+++
@@ -1,4 +0,0 @@
-export const API_CONFIG = {
-    development: 'http://localhost:8800/api',
-    production: 'https://cabinet.kiut.uz/api',
-};
